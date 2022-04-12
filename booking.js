b14d0ba3fdc36f7ae675ed8c1a4193a83c093341
saramsa2 <hwangc10@myunitec.ac.nz>
2022-04-12 20:21:03 +1200

    V0.02

--- booking.js
+++ booking.js
@@ -1,14 +1,28 @@
+function myPrevious()
+{
+    document.getElementById("progress").value--;
+    myProgress();
+}
+
+function myNext()
+{
+    document.getElementById("progress").value++;
+    myProgress();
+}
+
 function openPage1()
 {
+    document.getElementById('bookingHeadBox').style.display ="";
     document.getElementById('page1').style.display = "";
     document.getElementById('page2').style.display = "none";
-    document.getElementById('page2').style.display = "none";
-    document.getElementById('page2').style.display = "none";
+    document.getElementById('page3').style.display = "none";
+    document.getElementById('page4').style.display = "none";
     document.getElementById('pageSummary').style.display = "none";
 }
 
 function openPage2()
 {
+    document.getElementById('bookingHeadBox').style.display ="";
     document.getElementById('page1').style.display = "none";
     document.getElementById('page2').style.display = "";
     document.getElementById('page3').style.display = "none";
@@ -18,6 +32,7 @@ function openPage2()
 
 function openPage3()
 {
+    document.getElementById('bookingHeadBox').style.display ="";
     document.getElementById('page1').style.display = "none";
     document.getElementById('page2').style.display = "none";
     document.getElementById('page3').style.display = "";
@@ -27,6 +42,7 @@ function openPage3()
 
 function openPage4()
 {
+    document.getElementById('bookingHeadBox').style.display ="";
     document.getElementById('page1').style.display = "none";
     document.getElementById('page2').style.display = "none";
     document.getElementById('page3').style.display = "none";
@@ -36,12 +52,96 @@ function openPage4()
 
 function openPageSummary()
 {
-    document.getElementById('page1').style.display = "none";
-    document.getElementById('page2').style.display = "none";
-    document.getElementById('page3').style.display = "none";
-    document.getElementById('page4').style.display = "none";
-    document.getElementById('pageSummary').style.display = "";
+    if(validateInput())
+    {
+        document.getElementById('bookingHeadBox').style.display ="none";
+        document.getElementById('page1').style.display = "none";
+        document.getElementById('page2').style.display = "none";
+        document.getElementById('page3').style.display = "none";
+        document.getElementById('page4').style.display = "none";
+        document.getElementById('pageSummary').style.display = "";    
+    }
+    else
+    {
+        alert("Please, enter every inputs.");
+    }
+    
 }
+
+function openPageSummit()
+{    
+    alert("Thank you for your booking.");
+    location.reload();
+}
+
+function myProgress()
+{
+    var numPage = document.getElementById("progress").value;
+    
+    switch(numPage){
+        case "1":
+            openPage1();
+            break;
+        case "2":
+            openPage2();
+            break;
+        case "3":
+            openPage3();
+            break;
+        case "4":
+            openPage4();
+            break;
+        case "5":
+            openPageSummit();
+            break;
+    }
+}
+
+function validateInput()
+{
+    try
+    {
+        if(document.getElementById("twoSingle").checked) document.getElementById("lbBedType").innerHTML = document.getElementById("twoSingle").value;
+        else if(document.getElementById("queenBed").checked) document.getElementById("lbBedType").innerHTML = document.getElementById("queenBed").value;
+        else if(document.getElementById("bunkBed").checked) document.getElementById("lbBedType").innerHTML = document.getElementById("bunkBed").value;
+
+        debugger
+        var xx = document.getElementById("rgArravalTime").value;
+        document.getElementById("lbTime").innerHTML = document.getElementById("rgArravalTime").value + " PM";
+        xx = document.getElementById("lbTime").innerHTML;
+        document.getElementById("lbNumRoom").innerHTML = document.getElementById("numRooms").value;
+        document.getElementById("lbBreakfast").innerHTML = document.getElementById("breakfast").value;    
+
+        document.getElementById("lbColour").style.backgroundColor = document.getElementById("favouriteColor").value;        
+
+        document.getElementById("lbName").innerHTML = document.getElementById("txtFirstName").value + " " + document.getElementById("txtLastName").value;
+        document.getElementById("lbEmail").innerHTML = document.getElementById("txtEmail").value;
+        document.getElementById("lbPhone").innerHTML = document.getElementById("txtPhoneNo").value;
+
+        document.getElementById("lbChoice").innerHTML = "";
+        if(document.getElementById("ckBabaCot").checked) document.getElementById("lbChoice").innerHTML += document.getElementById("ckBabaCot").value + "<br>";
+        if(document.getElementById("ckBicycle").checked) document.getElementById("lbChoice").innerHTML += document.getElementById("ckBicycle").value + "<br>";
+        if(document.getElementById("ckGoggles").checked) document.getElementById("lbChoice").innerHTML += document.getElementById("ckGoggles").value + "<br>";
+        if(document.getElementById("ckSurfBoard").checked) document.getElementById("lbChoice").innerHTML += document.getElementById("ckSurfBoard").value;
+
+        if((document.getElementById("inDay").value != "") && (document.getElementById("outDay").value != "") && 
+            (document.getElementById("numAdults").value + document.getElementById("numChildren").value > 0) &&
+            (document.getElementById("txtFirstName").value !="") && (document.getElementById("txtLastName").value !="") && 
+            (document.getElementById("txtEmail").value != "") && (document.getElementById("txtPhoneNo").value != ""))
+        {   
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    catch(err)
+    {
+        return false;
+    }
+}
+
 function calStayDays()
 {    
     var checkIn = document.getElementById("inDay").value;
@@ -54,6 +154,8 @@ function calStayDays()
             var checkOutDay = new Date(checkOut);
             var stayDay = (checkOutDay - checkInDay)/(1000*3600*24);            
             document.getElementById("visitNights").innerHTML = stayDay + " nights stay";
+            document.getElementById("lbCheckIn").innerHTML = checkIn;
+            document.getElementById("lbCheckOut").innerHTML = checkOut;
         }
         else
         {
@@ -68,7 +170,16 @@ function calVisitors()
     var children = document.getElementById("numChildren").value;
     var totalPp = Number(adults) + Number(children);
     document.getElementById("numVisit").innerHTML = "Total "+ totalPp + " people";
+    document.getElementById("lbVisitor").innerHTML = totalPp + " people";
 }
+
+function ArrivalTimeChange()
+{
+    document.getElementById("lbArrivalTime").innerHTML = document.getElementById("rgArravalTime").value + " PM";
+}
+
+
+/////////////////////// Task2 AD function  ////////////////////////////////////////////////////////
 
 function restartAd()
 {   
